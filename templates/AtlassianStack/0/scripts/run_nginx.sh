cdf085d9de7d9494108468dfc118e2fedca40581
Peter Reid <peter@reidweb.com>
2017-09-02 21:07:00 +0100

    Update env var templating

--- templates/AtlassianStack/0/scripts/run_nginx.sh
+++ templates/AtlassianStack/0/scripts/run_nginx.sh
@@ -2,5 +2,6 @@
 
 echo "################################## Run nginx"
 export DOLLAR='$'
+printf '%s\n' "${!ROOT_DOMAIN}"
 envsubst < ./src/configs/nginx/nginx.conf.template > /etc/nginx/nginx.conf # /etc/nginx/conf.d/default.conf
 nginx -g "daemon off;"
